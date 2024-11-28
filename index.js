50d867454789e805ba0a31b129737b583611ba00
adhizsrm <adhizsrm@gmail.com>
2024-11-28 13:59:58 +0530

    removing unwanted comments

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
+let myLeads = [] 
+const inputEl = document.getElementById("input-el") 
+const inputBtn = document.getElementById("input-btn") 
+const ulEl = document.getElementById("ul-el") 
 
-// ["lead1", "lead2"] or null
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-console.log(leadsFromLocalStorage)
-// 1. Check if leadsFromLocalStorage is truthy
-// 2. If so, set myLeads to its value and call renderLeads()
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") ) 
+console.log(leadsFromLocalStorage) 
 
 if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    renderLeads()
-} 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    renderLeads()
+   myLeads = leadsFromLocalStorage 
+   renderLead() 
+}
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value) 
+    inputEl.value = "" 
+    localStorage.setItem("myLeads",JSON.stringify(myLeads)) 
+    renderLead() 
 })
 
-function renderLeads() {
+function renderLead() {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `
-            <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
-                </a>
-            </li>
-        `
+    for(let i = 0; i < myLeads.length; i++) {
+        listItems += `<li><a target = "_blank" href = "${myLeads[i]}">${myLeads[i]}</a></li>`
     }
-    ulEl.innerHTML = listItems  
+    ulEl.innerHTML = listItems 
 }
