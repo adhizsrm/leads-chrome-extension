35373bba74d2e39a584ceb0efe747c9d35ca4bbc
adhizsrm <adhizsrm@gmail.com>
2024-11-28 16:22:18 +0530

    adding DELETE ALL button and it's functionality

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-let myLeads = [] 
-const inputEl = document.getElementById("input-el") 
-const inputBtn = document.getElementById("input-btn") 
-const ulEl = document.getElementById("ul-el") 
-
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") ) 
-console.log(leadsFromLocalStorage) 
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") );
+console.log(leadsFromLocalStorage);
 
 if (leadsFromLocalStorage) {
-   myLeads = leadsFromLocalStorage 
-   renderLead() 
+   myLeads = leadsFromLocalStorage;
+   renderLead();
 }
 
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear();
+    myLeads = [];
+})
+
 inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value) 
-    inputEl.value = "" 
-    localStorage.setItem("myLeads",JSON.stringify(myLeads)) 
-    renderLead() 
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("myLeads",JSON.stringify(myLeads));
+    renderLead();
 })
 
 function renderLead() {
-    let listItems = ""
+    let listItems = "";
     for(let i = 0; i < myLeads.length; i++) {
-        listItems += `<li><a target = "_blank" href = "${myLeads[i]}">${myLeads[i]}</a></li>`
+        listItems += `<li><a target = "_blank" href = "${myLeads[i]}">${myLeads[i]}</a></li>`;
     }
-    ulEl.innerHTML = listItems 
+    ulEl.innerHTML = listItems;
 }
