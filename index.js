889453c66ca3a39239dcddb633a5256fefe350fa
adhizsrm <adhizsrm@gmail.com>
2024-11-28 13:47:11 +0530

    fixing errors related to localStorage rendering

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
-let myLeads = [];
-const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById("input-btn");
-const ulEl = document.getElementById("ul-el");
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-console.log(leadsFromLocalStorage);
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
-if(leadsFromLocalStorage) {
-   myLeads = leadsFromLocalStorage;
-   renderLead();
-}
+// ["lead1", "lead2"] or null
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+console.log(leadsFromLocalStorage)
+// 1. Check if leadsFromLocalStorage is truthy
+// 2. If so, set myLeads to its value and call renderLeads()
 
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value);
-    inputEl.value = "";
-    localStorage.setItem("myLeads",JSON.stringify("myLeads"));
-    renderLead();
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+} 
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    renderLeads()
 })
 
-function renderLead() {
+function renderLeads() {
     let listItems = ""
-    for(let i = 0 ; i < myLeads.length ; i++) {
-        listItems += `<li> <a target = "_blank" href = "${myLeads[i]}"> ${myLeads[i]} </a></li>`
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
     }
-    ulEl.innerHTML = listItems;
+    ulEl.innerHTML = listItems  
 }
